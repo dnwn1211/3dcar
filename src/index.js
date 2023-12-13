c34224bddd310ded8974c32592e369b81ea10b84
dnwn1211 <dnwn3027@gmail.com>
2023-12-14 00:09:43 +0900

    Fix line endings

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import { Scene } from "./components/Scene";
+import { Physics } from '@react-three/cannon';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Use createRoot to render the app
+createRoot(document.getElementById("root")).render(
+  // Use Canvas from @react-three/fiber
+  <Canvas>
+    <Physics broadphase='SAP' gravity={[0,-2.6,0]}>
+      <Scene />
+    </Physics>
+  </Canvas>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
